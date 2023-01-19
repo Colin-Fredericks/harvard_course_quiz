87038cf137228ceb38c84b64fcc2e1b4db5e32e4
Colin-Fredericks <Colin-Fredericks@users.noreply.github.com>
2023-01-19 15:11:34 -0500

    Working on building HTML
    Currently having loading concurrency issues.

--- js/main.js
+++ js/main.js
@@ -10,20 +10,70 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+getGoing();
+// TODO: the rest of the code is firing before the data is loaded.
+// Need to wait for the data to load before doing anything else.
+function getGoing() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let path = [];
+        let data = yield readData('Course_Quiz.txt');
+        let body = document.querySelector('body');
+        // This is just to test that the script is loading. Take out later.
+        body.insertBefore(document.createElement('hr'), body.firstChild);
+        body.insertBefore(constructHTML(data), body.firstChild);
+        body.insertBefore(makeBreadcrumbs(data, path), body.firstChild);
+        setupLinkListeners();
+        // No need to set breadcrumb listeners on the first page
+    });
+}
 /**
- * @description: Inserts data into a nested structure.
- * @param {Object} data_structure The nested structure to insert into.
- * @param {Array} path The path to the item to insert into.
- * @param {string} key The key to insert.
- * @param {any} value The value to insert. Can be an object.
+ * @description Reads in data from the data file with custom format.
+ * @param {string} filename The name of the file to read.
+ * @returns {Object} The data object.
  */
-function insertData(data_structure, path, key, value) {
-    console.debug('inserting ' + key + ': ' + value + ' into ' + path);
-    let current = data_structure;
-    for (let i = 0; i < path.length; i++) {
-        current = current[path[i]];
-    }
-    current[key] = value;
+function readData(filename) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Read in the file
+        fetch('data/' + filename)
+            .then((response) => response.text())
+            .then(function (data) {
+            return __awaiter(this, void 0, void 0, function* () {
+                // console.log(data);
+                let data_structure = yield processData(data);
+                console.log(data_structure);
+                // Parse the data into an object
+                return data;
+            });
+        })
+            .catch((error) => {
+            console.log(error);
+            return 'no data';
+        });
+        return 'no data';
+    });
+}
+/**
+ * @description Processes the data from a file into a nested structure.
+ * @param data The data to process, in text form.
+ * @returns A nested structure of objects containing the data.
+ */
+function processData(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let data_structure = {};
+        let lines = data.split('\n');
+        // Get the depth of each line
+        let grid = lines.map(function (line) {
+            return {
+                depth: line.split('\t').length - 1,
+                text: line.trim(),
+            };
+        });
+        // Remove empty lines
+        grid = grid.filter((line) => line.text.length > 0);
+        // console.debug(grid);
+        // Build the nested structure.
+        return buildStructure(grid);
+    });
 }
 /****************************
  * Here's what the structure should look like:
@@ -54,16 +104,16 @@ function insertData(data_structure, path, key, value) {
  * @returns {Object} The nested structure of the data.
  */
 function buildStructure(grid) {
-    let data_structure = { "name": "", "data": {}, "contents": {} };
+    let data_structure = { name: '', data: {}, contents: {} };
     let path = [];
     let last_depth = 0;
     for (let i = 0; i < grid.length; i++) {
         // Get the key and value
-        let [key, value] = grid[i]
-            .text
+        let [key, value] = grid[i].text
             .split(':')
             .map((item) => item.trim());
-        console.debug('line = ' + key + ': ' + value);
+        key = key.toLowerCase();
+        // console.debug('line = ' + key + ': ' + value);
         if (i === 0) {
             data_structure.name = key;
             continue;
@@ -77,7 +127,7 @@ function buildStructure(grid) {
             }
         }
         // If there's a key-value pair, add it to the data
-        if (typeof value !== "undefined") {
+        if (typeof value !== 'undefined') {
             path.push('data');
             insertData(data_structure, path, key, value);
             path.pop();
@@ -87,95 +137,17 @@ function buildStructure(grid) {
             // Add the item to the contents
             path.push('contents');
             insertData(data_structure, path, key, {
-                "name": key,
-                "data": {},
-                "contents": {}
+                name: key,
+                data: {},
+                contents: {},
             });
             // Add the item to the path
             path.push(key);
         }
-        console.debug(data_structure);
+        // console.debug(data_structure);
         last_depth = grid[i].depth;
     }
     return data_structure;
-}
-/* Older version of buildStructure
-function buildStructure(grid: any[], current_depth = 0): any {
-  let data_structure: any = {};
-  let current_line = grid.shift();
-
-  // TODO: something is wrong here. I'm iterating through the
-  // lines, but I think I'm overwriting the data_structure.
-
-  while (current_line.depth >= current_depth) {
-    let key = current_line.text.split(':');
-    console.log(key);
-
-    if (key.length === 1) {
-      data_structure["contents"] = {
-        [key[0].trim()]: buildStructure(grid, current_depth + 1)
-      };
-    } else {
-      data_structure[key[0].trim()] = key[1].trim();
-    }
-    current_line = grid.shift();
-
-    // Stop if we've reached the end of the file
-    if (typeof current_line === 'undefined') {
-      break;
-    }
-  }
-  return data_structure;
-}
-*/
-/**
- * @description Processes the data from a file into a nested structure.
- * @param data The data to process, in text form.
- * @returns A nested structure of objects containing the data.
- */
-function processData(data) {
-    return __awaiter(this, void 0, void 0, function* () {
-        let data_structure = {};
-        let lines = data.split('\n');
-        // Get the depth of each line
-        let grid = lines.map(function (line) {
-            return {
-                depth: line.split('\t').length - 1,
-                text: line.trim(),
-            };
-        });
-        // Remove empty lines
-        grid = grid.filter((line) => line.text.length > 0);
-        // console.debug(grid);
-        // Build the nested structure.
-        return buildStructure(grid);
-    });
-}
-/**
- * @description Reads in data from the data file with custom format.
- * @param {string} filename The name of the file to read.
- * @returns {Object} The data object.
- */
-function readData(filename) {
-    return __awaiter(this, void 0, void 0, function* () {
-        // Read in the file
-        fetch('data/' + filename)
-            .then((response) => response.text())
-            .then(function (data) {
-            return __awaiter(this, void 0, void 0, function* () {
-                console.log(data);
-                let data_structure = yield processData(data);
-                console.log(data_structure);
-                // Parse the data into an object
-                return data;
-            });
-        })
-            .catch((error) => {
-            console.log(error);
-            return 'no data';
-        });
-        return 'no data';
-    });
 }
 /**
  * @description Slides an element to the left while fading it out.
@@ -195,26 +167,91 @@ function slideTransition(element, direction, in_out) {
 }
 /**
  * @description: Constructs the HTML for the data.
- * @param {Object} data The data object at the current level of hierarchy.
+ * @param {Object} data A slice of the total data object.
  * @returns {HTMLElement} The HTML element containing the title, question, and cards.
  */
 function constructHTML(data) {
-    // Get the header
-    // Get the question
-    // Get the options
+    console.debug('constructHTML');
+    console.debug(data);
+    // Make the <main> tag.
+    let main = document.createElement('main');
+    let divider1 = document.createElement('div');
+    divider1.classList.add('divider');
+    let header = document.createElement('div');
+    header.id = 'header';
+    header.classList.add('row', 's12');
+    let header_text = document.createElement('h3');
+    header_text.innerText = data.data.title;
+    header.appendChild(header_text);
+    let question = document.createElement('div');
+    question.id = 'question';
+    question.classList.add('row', 's12', 'center-align');
+    let question_text = document.createElement('h3');
+    question_text.innerText = data.text;
+    question.appendChild(question_text);
+    let options = document.createElement('div');
+    options.id = 'options';
+    question.classList.add('row', 's12', 'center-align');
+    for (let key in data.contents) {
+        let card = createCard(data.contents[key], Object.keys(data.contents).length);
+        options.appendChild(card);
+    }
+    let divider2 = document.createElement('div');
+    divider2.classList.add('divider');
     // Put them all together
-    return document.createElement('div');
+    main.appendChild(divider1);
+    main.appendChild(header);
+    main.appendChild(question);
+    main.appendChild(options);
+    main.appendChild(divider2);
+    return main;
+}
+/**
+ * @description: Creates an option card.
+ * @param {Object} data A slice of the total data object.
+ * @returns {HTMLElement} The HTML element containing a single card.
+ */
+function createCard(data, num_cards) {
+    console.debug('createCard');
+    let width = String(Math.round(12 / num_cards));
+    let card = document.createElement('div');
+    card.classList.add('col', 'm' + width); // Need to adjust m2/3/4/6 for number of cards
+    let link = document.createElement('a');
+    link.href = '#!';
+    let card_div = document.createElement('div');
+    card_div.classList.add('card', 'blue-grey', 'darken-1', 'hoverable');
+    let card_content = document.createElement('div');
+    card_content.classList.add('card-content', 'white-text');
+    let card_title = document.createElement('p');
+    card_title.classList.add('flow-text');
+    let card_text = document.createElement('p');
+    card_text.classList.add('flow-text', 'card-text');
+    card_title.innerText = data.title;
+    card_text.innerText = data.text;
+    card_content.appendChild(card_title);
+    card_content.appendChild(card_text);
+    card_div.appendChild(card_content);
+    link.appendChild(card_div);
+    card.appendChild(link);
+    return card;
 }
 /**
  * @description: Makes HTML breadcrumbs for our current location.
  * @param {Array} breadcrumbs The array of breadcrumbs (strings).
  * @returns {HTMLElement} The HTML element containing the breadcrumbs.
  */
-function makeBreadcrumbs(breadcrumbs) {
+function makeBreadcrumbs(data, path) {
+    console.debug('makeBreadcrumbs');
     // Create the breadcrumb container
     // Create the breadcrumb elements
     // Put them together
-    return document.createElement('div');
+    let breadcrumbs = document.createElement('nav');
+    breadcrumbs.id = 'breadcrumbs';
+    breadcrumbs.classList.add('row', 'red', 'darken-4');
+    let nav_wrapper = document.createElement('div');
+    nav_wrapper.classList.add('nav-wrapper');
+    nav_wrapper.innerText = 'Breadcrumbs placeholder';
+    return breadcrumbs;
 }
 /**
  * @description: Set up the link listeners.
@@ -242,12 +279,22 @@ function setupBreadcrumbListeners() {
     // Slide the current page to the right and remove it
     // Reset the listeners
 }
-let depth = 0;
-let data = readData('Course_Quiz.txt');
-let header_box = document.getElementById('header');
-let question_box = document.getElementById('questions');
-let option_box = document.getElementById('options');
-// Need to figure out how to tell it where we are.
-setupLinkListeners();
-// No need to set breadcrumb listeners on the first page
+/***********************
+ * UTILITY FUNCTIONS
+ ***********************/
+/**
+ * @description: Inserts data into a nested structure.
+ * @param {Object} data_structure The nested structure to insert into.
+ * @param {Array} path The path to the item to insert into.
+ * @param {string} key The key to insert.
+ * @param {any} value The value to insert. Can be an object.
+ */
+function insertData(data_structure, path, key, value) {
+    // console.debug('inserting ' + key + ': ' + value + ' into ' + path);
+    let current = data_structure;
+    for (let i = 0; i < path.length; i++) {
+        current = current[path[i]];
+    }
+    current[key] = value;
+}
 //# sourceMappingURL=main.js.map
--- source/main.ts
+++ source/main.ts
@@ -2,25 +2,69 @@
 // import 'css/materialize.min.css';
 // import 'css/materialize.min.js';
 
+getGoing();
+
+// TODO: the rest of the code is firing before the data is loaded.
+// Need to wait for the data to load before doing anything else.
+
+async function getGoing() {
+  let path = [];
+  let data = await readData('Course_Quiz.txt');
+  let body = document.querySelector('body');
+
+  // This is just to test that the script is loading. Take out later.
+  body.insertBefore(document.createElement('hr'), body.firstChild);
+
+  body.insertBefore(constructHTML(data), body.firstChild);
+  body.insertBefore(makeBreadcrumbs(data, path), body.firstChild);
+
+  setupLinkListeners();
+  // No need to set breadcrumb listeners on the first page
+}
+
 /**
- * @description: Inserts data into a nested structure.
- * @param {Object} data_structure The nested structure to insert into.
- * @param {Array} path The path to the item to insert into.
- * @param {string} key The key to insert.
- * @param {any} value The value to insert. Can be an object.
+ * @description Reads in data from the data file with custom format.
+ * @param {string} filename The name of the file to read.
+ * @returns {Object} The data object.
  */
-function insertData(
-  data_structure: any,
-  path: Array<string>,
-  key: string,
-  value: any
-): void {
-  console.debug('inserting ' + key + ': ' + value + ' into ' + path);
-  let current = data_structure;
-  for (let i = 0; i < path.length; i++) {
-    current = current[path[i]];
-  }
-  current[key] = value;
+async function readData(filename: string): Promise<string> {
+  // Read in the file
+  fetch('data/' + filename)
+    .then((response) => response.text())
+    .then(async function (data) {
+      // console.log(data);
+      let data_structure = await processData(data);
+      console.log(data_structure);
+      // Parse the data into an object
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+      return 'no data';
+    });
+  return 'no data';
+}
+
+/**
+ * @description Processes the data from a file into a nested structure.
+ * @param data The data to process, in text form.
+ * @returns A nested structure of objects containing the data.
+ */
+async function processData(data: string): Promise<any> {
+  let data_structure: any = {};
+  let lines = data.split('\n');
+  // Get the depth of each line
+  let grid = lines.map(function (line) {
+    return {
+      depth: line.split('\t').length - 1,
+      text: line.trim(),
+    };
+  });
+  // Remove empty lines
+  grid = grid.filter((line) => line.text.length > 0);
+  // console.debug(grid);
+  // Build the nested structure.
+  return buildStructure(grid);
 }
 
 /****************************
@@ -64,7 +108,8 @@ function buildStructure(grid: any[]): any {
       .split(':')
       .map((item: string) => item.trim());
 
-    console.debug('line = ' + key + ': ' + value);
+    key = key.toLowerCase();
+    // console.debug('line = ' + key + ': ' + value);
 
     if (i === 0) {
       data_structure.name = key;
@@ -97,56 +142,11 @@ function buildStructure(grid: any[]): any {
       // Add the item to the path
       path.push(key);
     }
-    console.debug(data_structure);
+    // console.debug(data_structure);
     last_depth = grid[i].depth;
   }
 
   return data_structure;
-}
-
-/**
- * @description Processes the data from a file into a nested structure.
- * @param data The data to process, in text form.
- * @returns A nested structure of objects containing the data.
- */
-async function processData(data: string): Promise<any> {
-  let data_structure: any = {};
-  let lines = data.split('\n');
-  // Get the depth of each line
-  let grid = lines.map(function (line) {
-    return {
-      depth: line.split('\t').length - 1,
-      text: line.trim(),
-    };
-  });
-  // Remove empty lines
-  grid = grid.filter((line) => line.text.length > 0);
-  // console.debug(grid);
-  // Build the nested structure.
-  return buildStructure(grid);
-}
-
-/**
- * @description Reads in data from the data file with custom format.
- * @param {string} filename The name of the file to read.
- * @returns {Object} The data object.
- */
-async function readData(filename: string): Promise<string> {
-  // Read in the file
-  fetch('data/' + filename)
-    .then((response) => response.text())
-    .then(async function (data) {
-      console.log(data);
-      let data_structure = await processData(data);
-      console.log(data_structure);
-      // Parse the data into an object
-      return data;
-    })
-    .catch((error) => {
-      console.log(error);
-      return 'no data';
-    });
-  return 'no data';
 }
 
 /**
@@ -171,44 +171,14 @@ function slideTransition(
 }
 
 /**
- * @description: Creates an option card.
- * @param {Object} data A slice of the total data object.
- * @returns {HTMLElement} The HTML element containing a single card.
- */
-function createCard(data: any, num_cards: number): HTMLElement {
-  let width = String(Math.round(12 / num_cards));
-
-  let card = document.createElement('div');
-  card.classList.add('col', 'm'+width); // Need to adjust m2/3/4/6 for number of cards
-  let link = document.createElement('a');
-  link.href = '#!';
-  let card_div = document.createElement('div');
-  card_div.classList.add('card', 'blue-grey', 'darken-1', 'hoverable');
-  let card_content = document.createElement('div');
-  card_content.classList.add('card-content', 'white-text');
-  let card_title = document.createElement('p');
-  card_title.classList.add('flow-text');
-  let card_text = document.createElement('p');
-  card_text.classList.add('flow-text', 'card-text');
-
-  card_title.innerText = data.title;
-  card_text.innerText = data.text;
-
-  card_content.appendChild(card_title);
-  card_content.appendChild(card_text);
-  card_div.appendChild(card_content);
-  link.appendChild(card_div);
-  card.appendChild(link);
-
-  return card;
-}
-
-/**
  * @description: Constructs the HTML for the data.
  * @param {Object} data A slice of the total data object.
  * @returns {HTMLElement} The HTML element containing the title, question, and cards.
  */
 function constructHTML(data: any): HTMLElement {
+  console.debug('constructHTML');
+  console.debug(data);
+
   // Make the <main> tag.
   let main = document.createElement('main');
 
@@ -219,12 +189,14 @@ function constructHTML(data: any): HTMLElement {
   header.id = 'header';
   header.classList.add('row', 's12');
   let header_text = document.createElement('h3');
+  header_text.innerText = data.data.title;
   header.appendChild(header_text);
 
   let question = document.createElement('div');
   question.id = 'question';
   question.classList.add('row', 's12', 'center-align');
   let question_text = document.createElement('h3');
+  question_text.innerText = data.text;
   question.appendChild(question_text);
 
   let options = document.createElement('div');
@@ -249,7 +221,41 @@ function constructHTML(data: any): HTMLElement {
   main.appendChild(options);
   main.appendChild(divider2);
 
-  return document.createElement('div');
+  return main;
+}
+
+/**
+ * @description: Creates an option card.
+ * @param {Object} data A slice of the total data object.
+ * @returns {HTMLElement} The HTML element containing a single card.
+ */
+function createCard(data: any, num_cards: number): HTMLElement {
+  console.debug('createCard');
+  let width = String(Math.round(12 / num_cards));
+
+  let card = document.createElement('div');
+  card.classList.add('col', 'm' + width); // Need to adjust m2/3/4/6 for number of cards
+  let link = document.createElement('a');
+  link.href = '#!';
+  let card_div = document.createElement('div');
+  card_div.classList.add('card', 'blue-grey', 'darken-1', 'hoverable');
+  let card_content = document.createElement('div');
+  card_content.classList.add('card-content', 'white-text');
+  let card_title = document.createElement('p');
+  card_title.classList.add('flow-text');
+  let card_text = document.createElement('p');
+  card_text.classList.add('flow-text', 'card-text');
+
+  card_title.innerText = data.title;
+  card_text.innerText = data.text;
+
+  card_content.appendChild(card_title);
+  card_content.appendChild(card_text);
+  card_div.appendChild(card_content);
+  link.appendChild(card_div);
+  card.appendChild(link);
+
+  return card;
 }
 
 /**
@@ -257,11 +263,19 @@ function constructHTML(data: any): HTMLElement {
  * @param {Array} breadcrumbs The array of breadcrumbs (strings).
  * @returns {HTMLElement} The HTML element containing the breadcrumbs.
  */
-function makeBreadcrumbs(breadcrumbs: string[]): HTMLElement {
+function makeBreadcrumbs(data: object, path: string[]): HTMLElement {
+  console.debug('makeBreadcrumbs');
   // Create the breadcrumb container
   // Create the breadcrumb elements
   // Put them together
-  return document.createElement('div');
+  let breadcrumbs = document.createElement('nav');
+  breadcrumbs.id = 'breadcrumbs';
+  breadcrumbs.classList.add('row', 'red', 'darken-4');
+  let nav_wrapper = document.createElement('div');
+  nav_wrapper.classList.add('nav-wrapper');
+
+  nav_wrapper.innerText = 'Breadcrumbs placeholder';
+  return breadcrumbs;
 }
 
 /**
@@ -292,13 +306,27 @@ function setupBreadcrumbListeners(): void {
   // Reset the listeners
 }
 
-let depth = 0;
-let data = readData('Course_Quiz.txt');
-let header_box = document.getElementById('header');
-let question_box = document.getElementById('questions');
-let option_box = document.getElementById('options');
+/***********************
+ * UTILITY FUNCTIONS
+ ***********************/
 
-// Need to figure out how to tell it where we are.
-
-setupLinkListeners();
-// No need to set breadcrumb listeners on the first page
+/**
+ * @description: Inserts data into a nested structure.
+ * @param {Object} data_structure The nested structure to insert into.
+ * @param {Array} path The path to the item to insert into.
+ * @param {string} key The key to insert.
+ * @param {any} value The value to insert. Can be an object.
+ */
+function insertData(
+  data_structure: any,
+  path: Array<string>,
+  key: string,
+  value: any
+): void {
+  // console.debug('inserting ' + key + ': ' + value + ' into ' + path);
+  let current = data_structure;
+  for (let i = 0; i < path.length; i++) {
+    current = current[path[i]];
+  }
+  current[key] = value;
+}
